8643de493e0c1ce1613be50d934cbc234c50656a
nate chesley <nate@almanac.local>
2017-03-14 11:15:30 +0200

    add post to server

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 
+app.post('/memoriesdb', function(req, res, next) {
+  Memory.create(req.body, function(err, mem) {
+    if (err) {
+      console.error(err)
+      return next(err);
+    } else {
+      res.json(mem);
+    }
+  });
+});
+
 app.listen(8000, function() {
   console.log("Memory app started. Oof al ze!")
 });
