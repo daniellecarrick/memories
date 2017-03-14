36b3cd20a7288d1d3e6acaa5522a696362ebf41a
nate chesley <nate@almanac.local>
2017-03-14 11:22:27 +0200

    tidy server so far for legibility

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
+//  packages & modules  >
 var express = require('express');
-var app = express();
-
 var mongoose = require('mongoose');
 var Memory = require("./models/memoryModel");
+var bodyParser = require('body-parser');
+
+var app = express();
 mongoose.connect("mongodb://localhost/memoriesdb");
 
-var bodyParser = require('body-parser');
+//  middleware  >
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 
+//  api routes  >
 app.post('/memoriesdb', function(req, res, next) {
   Memory.create(req.body, function(err, mem) {
     if (err) {
@@ -23,6 +25,7 @@ app.post('/memoriesdb', function(req, res, next) {
   });
 });
 
+//  run server  >
 app.listen(8000, function() {
   console.log("Memory app started. Oof al ze!")
 });
