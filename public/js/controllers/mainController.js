4482570a019e15e41ee51ecec4c379a3f3014ca4
einarzb <einarzb@gmail.com>
2017-03-15 17:21:11 +0200

    filter by location works

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,9 +1,10 @@
 app.controller('mainController', function($scope, memFactory) {
-
+$scope.thanks = false; //hides thank you
   $scope.addMem = function(newMem) {
     //&& $scope.posNeg && $scope.age && $scope.location
    // if($scope.age > 0){
       memFactory.addMem(newMem).then(function(mem) {
+      $scope.thanks = true;
       $scope.memoriesdb.push(mem);
       console.log(mem.body, mem.age);
     });
